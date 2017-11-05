133c099f04d81c0cc939c509e57563a1ae8c72fc
lelea2 <daomaikhanh1988@yahoo.com>
2017-11-05 11:48:26 -0800

    Initial check in

--- custom-config/index.js
+++ custom-config/index.js
@@ -1,13 +1,3 @@
-const nconf = require('nconf');
-const environment = process.env.NODE_ENV || 'development'; // default to development
+const config = require('./lib/configuration');
 
-function Config() {
-  nconf.file(environment, './config/' + environment.toLowerCase() + '.json');
-  nconf.file('default', './config/default.json');
-}
-
-Config.prototype.get = function(key) {
-  return nconf.get(key);
-};
-
-module.exports = new Config();
+module.exports = config;
